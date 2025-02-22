971d128ab01540a95b580346d8f56a2f865d7ab7
Sujal Mane <sujalmane896@gmail.com>
2025-02-23 01:59:42 +0530

    Update server.js

--- server.js
+++ server.js
@@ -48,7 +48,5 @@ app.get('/', (req, res) => {
 });
 
 // Start Server
-const PORT = 3000;
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
-});
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
